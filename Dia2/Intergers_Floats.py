0a361abed98608a54c1ed732d042b077b2cb1ffd
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-01 18:57:50 -0600

    numeros

---
+++ Dia2/Intergers_Floats.py
@@ -0,0 +1,6 @@
+edad = input("Dime tu edad:")
+
+print("Tu edad es: "+ edad)
+
+nueva_edad = 1 + int(edad)
+print("Vas a cumplir " + str(nueva_edad))
---
+++ Dia2/variables.py
@@ -0,0 +1,6 @@
+edad = 30
+edad2 = 50
+
+frase = edad2 + edad
+
+print("Tu edad es: " + str(frase) )
