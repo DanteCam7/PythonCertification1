3fcc404710041d3a06bd7b2dfd9056c1774060f0
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-02 00:34:25 -0600

    PROPIEDADES

---
+++ Dia3/Propiedades.py
@@ -0,0 +1,9 @@
+poema = """Mil pequeños tejones de miels
+como si hirviera
+el color del agua"""  #SALTO DE LINEA CON 3 comillas
+
+print(poema)
+
+print("pez" in poema) #devuelve true o false si está en la variable PUEDE SER """""""NOT IN"""""""
+
+print(len(poema)) #cantidad de caracteres
