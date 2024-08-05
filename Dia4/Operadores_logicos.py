b277f8d8b25a45b7ed7ebcd971c970aeb4a1972d
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-05 10:15:28 -0600

    operadores comparar

---
+++ Dia4/Operadores_logicos.py
@@ -0,0 +1,10 @@
+mi_bool=4<5 and 5<6
+mi_bool2= 10==10 or 3==2
+print(mi_bool)
+
+texto = "esta frase es breve"
+mi_bool3= ('frase' in texto) or ('python' in texto)
+print(mi_bool3)
+
+mi_bool4= not ('a' == 'a')
+print(mi_bool4)
