0a7fe7f1a7b5dd87d9f9f86b8ceb30f4bb0825c5
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-06 11:20:12 -0600

    loop while

---
+++ Dia4/Loop_While.py
@@ -0,0 +1,26 @@
+monedas = 5
+while monedas > 0:
+    print(f"Tengo {monedas} monedas")
+    monedas -= 1
+else:print("No tengo más dinero")
+
+respuesta = 's'
+
+while respuesta != 'n':
+    respuesta = input('Quieres seguir? (s/n)')
+else:
+    print('Gracias')
+
+#########################
+while respuesta == 's':
+    pass
+
+print("hola")
+######################
+
+nombre = input("Tu nombre: ")
+
+for letra in nombre:
+    if letra == 'r':
+        continue #break
+    print(letra)
