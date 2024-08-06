146c8b4fb023e4a063ab25922fe524d194b08497
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-06 17:04:16 -0600

    min_max

---
+++ Dia4/Min_Max.py
@@ -0,0 +1,10 @@
+menor = min (58,96,72,64,35)
+mayor = max (58,96,72,64,35)
+print(menor)
+print(mayor)
+
+lista = menor = (58,96,72,64,35)
+print(min(lista))
+
+dic = {'C1':45, 'C2':11}
+print(min(dic.values()))
