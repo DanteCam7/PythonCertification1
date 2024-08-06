f6963936d8e3fa769d9392ae5281d48d14659cc7
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-06 16:06:51 -0600

    Range

---
+++ Dia4/Range.py
@@ -0,0 +1,10 @@
+lista = list(range(1,101))
+print(lista)
+
+for numero in range(20,31,4):
+    print(numero)
+
+suma_cuadrados = 0
+
+for numero in range(1,16)
+    suma_cuadrados = suma_cuadrados + numero**2
