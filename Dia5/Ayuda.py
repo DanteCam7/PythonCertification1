18a233c2d8a237d3f5ff6ffa9f47deda10f6f706
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-07 11:29:22 -0600

    Leer Documentacion

---
+++ Dia5/Ayuda.py
@@ -0,0 +1,16 @@
+dic = {'clave1':100,'clave2':500}
+
+a = dic.popitem()
+print(a)
+print(dic)
+
+print(",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#".lstrip(",:%_#"))
+
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+frutas.insert(3,"naranja")
+
+marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+
+marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+
+conjuntos_aislados = marcas_tv.isdisjoint(marcas_smartphones)
