d0fb6970e77037794db6e8558bb327afea3e2803
DanteCam7 <dante.cibarra7@gmail.com>
2024-08-07 11:40:52 -0600

    Functions

---
+++ Dia5/Funciones.py
@@ -0,0 +1,8 @@
+def saludar_persona (nombre):
+    '''
+    Esta función sirve para saludar a las personas
+    otra linea
+    '''
+    print('Hola ' + nombre)
+
+saludar_persona(input('Nombre porfavor: '))
