62441781244743dce8f5d9b3c8ef0d2649f73b78
DanteCam7 <dante.cibarra7@gmail.com>
2024-09-02 11:52:14 -0600

    Error_Handling_Pylint

---
+++ Dia8 Modules_Packages/archive1.py
@@ -0,0 +1,12 @@
+"""
+This is a module that print something
+Using Pylint on the CMD to look for errors
+"""
+
+
+def function():
+    number1 = 500
+    print(number1)
+
+
+function()
---
+++ Dia8 Modules_Packages/practica_pylint.py
@@ -0,0 +1,12 @@
+"""
+LEARNING PYLINT
+"""
+
+
+def sumar(number1, number2):
+    return number1+number2
+
+
+SUMA = sumar(5, 7)
+
+print(f"The answer is {SUMA}")
