2ac72a8c43781e5cc31f0979ef866669c6a879e3
DanteCam7 <dante.cibarra7@gmail.com>
2024-09-02 11:18:32 -0600

    PackagesP3

---
+++ exercise.py
@@ -0,0 +1,9 @@
+from Package_Dante import addition_and_subtraction
+
+addition_and_subtraction.addition(1,1)
+
+addition_and_subtraction.substraction(1,2)
+
+from Package_Dante.Subpackage import greet
+
+greet.hello()
