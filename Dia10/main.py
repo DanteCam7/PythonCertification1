ad704c23eadcb5129032c9c31015f158f30c01c5
DanteCam7 <dante.cibarra7@gmail.com>
2024-09-19 17:42:04 -0600

    game space invasion finalizado1

--- Dia10/main.py
+++ Dia10/main.py
@@ -1,5 +1,9 @@
 import pygame
 import random
+import math
+from random import randint
+from pygame import mixer
+import io
 
 # Inicializar pygame
 pygame.init()
@@ -13,6 +17,10 @@ icono = pygame.image.load('ovni.png')
 pygame.display.set_icon(icono)
 fondo = pygame.image.load('fondo.jpg')
 
+# agregar musica
+mixer.music.load('Los Marcianos Llegaron Ya.mp3')
+mixer.music.play(-1)
+
 # Variables del Jugador
 img_jugador = pygame.image.load('cohete.png')
 jugador_x = 368
@@ -21,19 +29,59 @@ jugador_x_cambio = 0
 jugador_y_cambio = 0
 
 # Variables del Enemigo
-img_enemigo = pygame.image.load('enemigo.png')
-enemigo_x = random.randint(0, 736)
-enemigo_y = random.randint(50, 200)
-enemigo_x_cambio = 0.1
-enemigo_y_cambio = 25
+img_enemigo = []
+enemigo_x = []
+enemigo_y = []
+enemigo_x_cambio = []
+enemigo_y_cambio = []
+cantidad_enemigos = randint(8, 11)
+
+for e in range(cantidad_enemigos):
+    img_enemigo.append(pygame.image.load('enemigo.png'))
+    enemigo_x.append(random.randint(0, 736))
+    enemigo_y.append(random.randint(50, 200))
+    enemigo_x_cambio.append(0.2)
+    enemigo_y_cambio.append(30)
 
 # Variables de la bala
+balas = []
 img_bala = pygame.image.load('bullet.png')
 bala_x = 0
 bala_y = 500
 bala_x_cambio = 0
-bala_y_cambio = 0.2
+bala_y_cambio = 1
 bala_visible = False
+
+
+# fuente en bytes
+def fuente_bytes(fuente1):
+    with open(fuente1, 'rb') as f:
+        ttf_bytes = f.read()
+
+    return io.BytesIO(ttf_bytes)
+
+
+# puntaje
+puntaje = 0
+fuente_bytes_1 = fuente_bytes('FreeSansBold.ttf')
+fuente = pygame.font.Font('freesansbold.ttf', 32)
+texto_x = 10
+texto_y = 10
+
+
+# funcion mostrar puntaje
+def mostrar_puntaje(x, y):
+    texto = fuente.render(f'Puntaje: {puntaje}', True, (255, 255, 255))
+    pantalla.blit(texto, (x, y))
+
+
+# texto final de juego
+fuente_final = pygame.font.Font(fuente_bytes_1, 40)
+
+
+def texto_final():
+    mi_fuente_final = fuente_final.render('GAME OVER', True, (255, 255, 255))
+    pantalla.blit(mi_fuente_final, (250, 250))
 
 
 # Funcion jugador
@@ -42,14 +90,23 @@ def jugador(x, y):
 
 
 # Funcion enemigo
-def enemigo(x, y):
-    pantalla.blit(img_enemigo, (x, y))
+def enemigo(x, y, ene):
+    pantalla.blit(img_enemigo[ene], (x, y))
 
 
 def disparar_bala(x, y):
     global bala_visible
     bala_visible = True
     pantalla.blit(img_bala, (x+16, y+10))
+
+
+# funcion detectar colisiones
+def hay_colision(x1, y1, x2, y2):
+    distancia = math.sqrt(math.pow(x1 - x2, 2) + math.pow(y2 - y1, 2))
+    if distancia < 32:
+        return True
+    else:
+        return False
 
 
 # Loop del juego
@@ -73,7 +130,17 @@ while se_ejecuta:
             if evento.key == pygame.K_RIGHT:
                 jugador_x_cambio = 0.2
             if evento.key == pygame.K_SPACE:
-                disparar_bala(jugador_x, bala_y)
+                sonido_bala = mixer.Sound('disparo.mp3')
+                sonido_bala.play()
+                nueva_bala = {
+                    "x": jugador_x,
+                    "y": jugador_y,
+                    "velocidad": -5
+                }
+                balas.append(nueva_bala)
+                if not bala_visible:
+                    bala_x = jugador_x
+                    disparar_bala(bala_x, bala_y)
 
         # evento soltar flechas
         if evento.type == pygame.KEYUP:
@@ -90,25 +157,47 @@ while se_ejecuta:
         jugador_x = 736
 
     # modificar ubicación del enemigo
-    enemigo_x += enemigo_x_cambio
+    for e in range(cantidad_enemigos):
+
+        # fin del juego
+        if enemigo_y[e] > 450:
+            for k in range(cantidad_enemigos):
+                enemigo_y[k] = 1000
+            texto_final()
+            break
+        enemigo_x[e] += enemigo_x_cambio[e]
 
     # mantener dentro de bordes al enemigo
-    if enemigo_x <= 0:
-        enemigo_x_cambio = 0.1
-        enemigo_y += enemigo_y_cambio
-    elif enemigo_x >= 736:
-        enemigo_x_cambio = -0.1
-        enemigo_y += enemigo_y_cambio
+        if enemigo_x[e] <= 0:
+            enemigo_x_cambio[e] = 0.2
+            enemigo_y[e] += enemigo_y_cambio[e]
+        elif enemigo_x[e] >= 736:
+            enemigo_x_cambio[e] = -0.2
+            enemigo_y[e] += enemigo_y_cambio[e]
+
+        # colision
+        for bala in balas:
+            colision = hay_colision(enemigo_x[e], enemigo_y[e], bala["x"], bala["y"])
+            if colision:
+                sonido_colision = mixer.Sound('Golpe.mp3')
+                sonido_colision.play()
+                balas.remove(bala)
+                puntaje += 1
+                enemigo_x[e] = random.randint(0, 736)
+                enemigo_y[e] = random.randint(50, 200)
+                break
+
+        enemigo(enemigo_x[e], enemigo_y[e], e)
 
     # movimiento bala
-    if bala_visible:
-        disparar_bala(jugador_x, bala_y)
-        bala_y -= bala_y_cambio
+    for bala in balas:
+        bala["y"] += bala["velocidad"]
+        pantalla.blit(img_bala, (bala["x"] + 16, bala["y"] + 10))
+        if bala["y"] < 0:
+            balas.remove(bala)
 
     jugador(jugador_x, jugador_y)
-    enemigo(enemigo_x, enemigo_y)
+    mostrar_puntaje(texto_x, texto_y)
 
     # actualizar
     pygame.display.update()
-
-
